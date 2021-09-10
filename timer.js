73fff3b77192d3ea34386ed6bbfecc3d34c76835
EvgeniyKarpuhin <evgeniygalaxy4@gmail.com>
2021-09-11 00:21:54 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,29 +1,45 @@
-new CountdownTimer({
+class Timer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+
+        this.refs = {
+            clock: document.querySelector('#timer-1'),
+            daysTimer: document.querySelector(`${selector} span[data-value="days"]`),
+            hoursTimer: document.querySelector(`${selector} span[data-value="hours"]`),
+            minutesTimer: document.querySelector(`${selector} span[data-value="mins"]`),
+            secondsTimer: document.querySelector(`${selector} span[data-value="secs"]`),
+        };
+        this.start();
+    }
+    start() {
+        setInterval(() => {
+            const dataTime = this.targetDate - Date.now();
+            this.updateClock(this.showTime(dataTime));
+        }, 1000);
+    }
+
+    updateClock({ days, hours, mins, secs }) {
+        this.refs.daysTimer.innerHTML = days;
+        this.refs.hoursTimer.innerHTML = hours;
+        this.refs.minutesTimer.innerHTML = mins;
+        this.refs.secondsTimer.innerHTML = secs;
+    }
+    showTime(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs }
+    }
+}
+
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+
+const CountDownTimer = new Timer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2019'),
+    targetDate: new Date('Oct 1, 2021'),
   });
-
-  /*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
